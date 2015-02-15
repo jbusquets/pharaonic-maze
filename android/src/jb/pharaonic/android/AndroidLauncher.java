58c4fb53bca6ecd3f89e9f5a811f602483c57126
Jordi Busquets <jbusquets@gmail.com>
2015-02-15 18:01:58 +0100

    initial commit

---
+++ android/src/jb/pharaonic/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package jb.pharaonic.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import jb.pharaonic.PharaonicMaze;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new PharaonicMaze(), config);
+	}
+}
---
+++ desktop/src/jb/pharaonic/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package jb.pharaonic.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import jb.pharaonic.PharaonicMaze;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PharaonicMaze(), config);
+	}
+}
